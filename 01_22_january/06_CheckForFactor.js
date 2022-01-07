e8f91b33ef8884e881031a85e311ca019ff10e96
Oleksandr Vlasenko <oleksvlas@gmail.com>
2022-01-07 17:16:27 +0200

    Changed sixth task (07.01.2022)

--- 01_22_january/06_CheckForFactor.js
+++ 01_22_january/06_CheckForFactor.js
@@ -15,6 +15,5 @@ For example 2 is not a factor of 7 because: 7 % 2 = 1
 Note: base is a non-negative number, factor is a positive number.
  */
 function checkForFactor(base, factor) {
-    //  console.log(base%factor !== 0);
     return base % factor !== 0 ? false : true;
 }
