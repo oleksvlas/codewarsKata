d85a91cbad15e084607e269440b5ab8cef22c0da
Oleksandr Vlasenko <oleksvlas@gmail.com>
2022-01-08 21:17:23 +0200

    Finished the eighth task (08.01.22)

--- 01_22_january/08_SquareSum.js
+++ 01_22_january/08_SquareSum.js
@@ -3,7 +3,6 @@ Complete the square sum function so that it squares each number passed into it a
 
 For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 */
-
 function squareSum(numbers) {
   return numbers.reduce((total, el) => total + el ** 2, 0);
 }
