90a8f2add119328ac62335ffbf628f9d6126b3ad
Oleksandr Vlasenko <oleksvlas@gmail.com>
2022-01-13 15:50:26 +0200

    Finished the another thirteenth task (13.01.22)

--- 01_22_january/13_TheHighestProfitWins.js
+++ 01_22_january/13_TheHighestProfitWins.js
@@ -12,7 +12,6 @@ minMax([1])           == [1, 1]
 Remarks
 All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
 */
-
 function minMax(arr) {
   let arr2 = [0, 0];
   arr.forEach((el, index) => {
